87c5a6a034e0c381ac7b72f3e06718a3dadb358a
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-07-22 16:05:48 -0300

    Add files via upload

---
+++ switch/switch.js
@@ -0,0 +1,16 @@
+x=prompt("Digite um nomero");
+
+switch(x){
+	case "0":
+		alert("x eh Zero!!!");
+		break;
+	case "1":
+		alert("x eh um!!!");
+		break;
+	case "2":
+		alert("x eh Dois!!!");
+		break;
+	default:
+		alert("x não encontrado!!!");
+		break;
+}
