f3ab975749a2abe53ec2a61b2d36ef36a9c008a9
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-07-22 20:47:22 -0300

    Add files via upload

---
+++ Operações/script.js
@@ -0,0 +1,7 @@
+function somar(){
+	var campo1 = parseInt(document.getElementById("c1").value);
+	var campo2 = parseInt(document.getElementById("c2").value);
+	var res = campo1+campo2;
+
+	alert(res)
+}
