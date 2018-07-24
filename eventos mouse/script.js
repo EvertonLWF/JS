92f05b88aa8d9f9c753b90a3e704a423b1ebc65f
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-07-23 22:33:50 -0300

    Add files via upload

---
+++ eventos mouse/script.js
@@ -0,0 +1,25 @@
+function down(){//oonmousedown
+	console.log("Apertou o mouse");
+}
+function up(){// onmouseup
+	console.log("soltou o mouse");
+}
+function cima(){//onmouseover
+	console.log("passou o mouse");
+}
+function saiu(){//onmouseout
+	console.log("Tirou o mouse");
+}
+function move(){//onmousemove
+	console.log("mexeu o mouse");
+}
+function clik(){//onclick
+	console.log("clicou");
+}
+function text(){//oncontextmenu
+	console.log("botão direito");
+	return false;
+}
+function dbclick(){
+	console.log("duplo click");
+}
