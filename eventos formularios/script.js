97a85b35353e5786abaf39f3f714d8ee78b630c3
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-07-23 23:58:43 -0300

    Add files via upload

---
+++ eventos formularios/script.js
@@ -0,0 +1,9 @@
+function mudar(objeto){//onchange
+	console.log("selecionou: "+objeto.value);
+}
+function foco(){//onfocus
+	console.log("focou no campo 1");
+}
+function desfoco(){//onblur
+	console.log("desfocou no campo 1");
+}
